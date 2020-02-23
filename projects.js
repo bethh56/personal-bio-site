8d00723f303a08348fc42db4bab6a65a5d4aac19
Beth <bethhnielsen56@gmail.com>
2020-02-23 11:07:58 -0600

    wip

--- projects.js
+++ projects.js
@@ -4,7 +4,7 @@ const projects = [
     screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
     description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
-    available: true,
+    available: false,
     url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
     githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
     }
@@ -15,29 +15,33 @@ const printToDom = (divId, textToPrint) => {
     selctedDiv.innerHTML = textToPrint;
 }
 
-const createProjectCards = () => {
+const createProjectCards = (projectCards) => {
     let domString = "";
-    for (let i = 0; i < projects.length; i++) {
+    for (let i = 0; i < projectCards.length; i++) {
+        const filter = projectCards.filter(p => p.available === true);
+        console.log(filter);
         domString += `<div class="card mx-auto w-75 text-body" id=projectCard>`;
         domString +=  `<header>`;
-        domString +=    `<h1>${projects[i].title}</h1>`;
+        domString +=    `<h1>${projectCards[i].title}</h1>`;
         domString +=  `</header>`;
-        domString +=  `<img src="${projects[i].screenshot}"/>`;
+        domString +=  `<img src="${projectCards[i].screenshot}"/>`;
         domString +=  `<div>`;
-        domString +=     `<p>${projects[i].technologiesUsed}</p>`;
-        domString +=     `<p>${projects[i].available}</p>`;
-        domString +=     `<a href="${projects[i].url}">Link to Project</a>`;
+        domString +=     `<p>${projectCards[i].technologiesUsed}</p>`;
+        domString +=     `<div class="${projectCards[i].available}"></div>`;
+        domString +=     `<a href="${projectCards[i].url}">Link to Project</a>`;
         domString +=     `<br>`;
-        domString +=     `<a href="${projects[i].githuburl}">Link to GitHub</a>`; 
+        domString +=     `<a href="${projectCards[i].githuburl}">Link to GitHub</a>`; 
         domString +=  `</div>`;   
         domString += `</div>`
+        
     }
+
     printToDom('projectsToDom', domString);
 }
 
 
 const init = () => {
-    createProjectCards();    
+    createProjectCards(projects);    
 }
 
 init();
