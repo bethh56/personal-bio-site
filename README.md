703738b77b98faefd68c1dab587e2bb4911a9733
Beth <bethhnielsen56@gmail.com>
2020-02-23 12:03:34 -0600

    wip

--- README.md
+++ README.md
@@ -4,5 +4,11 @@
 #### 
 
 ## Screen Shots
+![image](screenshot(10).png)
 
-## How To Run
+
+## How to Run
+1. Clone down this repo
+1. Make sure you have http-server installed via npm.  If not, get it [here](https://www.npmjs.com/package/http-server)
+1. On your command line run `hs -p 9100`
+1. In your browser go to `http://localhost:9100`
--- projects.js
+++ projects.js
@@ -5,8 +5,26 @@ const projects = [
     description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
     technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
     available: false,
-    url: "https://github.com/nss-evening-cohort-8/js-part-deux", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
-    githubUrl: "https://github.com/nss-evening-cohort-8/js-part-deux"
+    url: "https://github.com/bethh56", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/bethh56"
+    },
+    {
+    title: "Another Project", 
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: true,
+    url: "https://github.com/bethh56", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/bethh56"
+    },
+    {
+    title: "Cool Project", 
+    screenshot: "http://gotoflashgames.com/files/file/033.jpg", 
+    description: "This is the best project", // A good project description includes 'the what', 'the why', and 'the how'.
+    technologiesUsed: "HTML, CSS, Vanilla JavaScript, Version Control with Github",
+    available: false,
+    url: "https://github.com/bethh56", // Towards the latter part of the class, you will learn how to host your projects and people will be able to view them live. Cool, right? Welp, until then, just use your GitHub link in this spot as well.
+    githubUrl: "https://github.com/bethh56"
     }
 ]
 
@@ -18,24 +36,22 @@ const printToDom = (divId, textToPrint) => {
 const createProjectCards = (projectCards) => {
     let domString = "";
     for (let i = 0; i < projectCards.length; i++) {
-        const filter = projectCards.filter(p => p.available === true);
-        console.log(filter);
-        domString += `<div class="card mx-auto w-75 text-body" id=projectCard>`;
-        domString +=  `<header>`;
-        domString +=    `<h1>${projectCards[i].title}</h1>`;
-        domString +=  `</header>`;
-        domString +=  `<img src="${projectCards[i].screenshot}"/>`;
-        domString +=  `<div>`;
-        domString +=     `<p>${projectCards[i].technologiesUsed}</p>`;
-        domString +=     `<div class="${projectCards[i].available}"></div>`;
-        domString +=     `<a href="${projectCards[i].url}">Link to Project</a>`;
-        domString +=     `<br>`;
-        domString +=     `<a href="${projectCards[i].githuburl}">Link to GitHub</a>`; 
-        domString +=  `</div>`;   
-        domString += `</div>`
-        
+        if (projectCards[i].available === true) {
+            domString += `<div class="card ml-3 mr-3 mb-4 text-body" id=projectCard>`;
+            domString +=  `<header>`;
+            domString +=    `<h1>${projectCards[i].title}</h1>`;
+            domString +=  `</header>`;
+            domString +=  `<img src="${projectCards[i].screenshot}"/>`;
+            domString +=  `<div>`;
+            domString +=     `<p>${projectCards[i].technologiesUsed}</p>`;
+            domString +=     `<div class="${projectCards[i].available}"></div>`;
+            domString +=     `<a href="${projectCards[i].url}">Link to Project</a>`;
+            domString +=     `<br>`;
+            domString +=     `<a href="${projectCards[i].githuburl}">Link to GitHub</a>`; 
+            domString +=  `</div>`;   
+            domString += `</div>`;
+        }    
     }
-
     printToDom('projectsToDom', domString);
 }
 
