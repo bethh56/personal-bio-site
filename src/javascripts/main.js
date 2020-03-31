26804afb794f9a85ab0ebd6a65be038ce06cb4a9
Beth <bethhnielsen56@gmail.com>
2020-03-31 14:59:55 -0500

    updated footer

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,7 +1,16 @@
 import projects from './components/projects';
 import '../styles/main.scss';
+import utils from './helpers/utils';
+
+const addDateToFooter = () => {
+  const date = new Date().getFullYear();
+  let domString = '';
+  domString += `<p class="col-xs-1 text-center text-white">Nielsen ${date}</p>`;
+  utils.printToDom('addMomentTimeStamp', domString);
+};
 
 const init = () => {
+  addDateToFooter();
   projects.createProjectCards();
 };
 
