5b4499329a552bf97c0111caddc39af49442ff0a
Beth <bethhnielsen56@gmail.com>
2020-04-06 19:33:04 -0500

    wip

--- src/javascripts/helpers/data/projectData.js
+++ src/javascripts/helpers/data/projectData.js
@@ -1,32 +1,4 @@
-const projects = [
-  {
-    title: 'Cool Project',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project',
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: false,
-    url: 'https://github.com/bethh56',
-    githubUrl: 'https://github.com/bethh56',
-  },
-  {
-    title: 'Cool Project',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project',
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: true,
-    url: 'https://github.com/bethh56',
-    githubUrl: 'https://github.com/bethh56',
-  },
-  {
-    title: 'Cool Project',
-    screenshot: 'http://gotoflashgames.com/files/file/033.jpg',
-    description: 'This is the best project',
-    technologiesUsed: 'HTML, CSS, Vanilla JavaScript, Version Control with Github',
-    available: false,
-    url: 'https://github.com/bethh56',
-    githubUrl: 'https://github.com/bethh56',
-  },
-];
+const projects = [];
 
 const getProjects = () => projects;
 
