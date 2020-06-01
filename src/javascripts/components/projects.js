54128a409d4900c90237a1c4d7523113bd330886
Beth <bethhnielsen56@gmail.com>
2020-05-31 23:30:10 -0500

    wip

--- src/javascripts/components/projects.js
+++ src/javascripts/components/projects.js
@@ -5,20 +5,18 @@ const createProjectCards = () => {
   projectData.getProjects()
     .then((projects) => {
       let domString = '';
-      console.error(projects.data);
       projects.forEach((projectCards) => {
         if (projectCards.available === true) {
-          domString += '<div class="projectCard col-4" id=projectCard>';
+          domString += '<div class="projectCard col-3" id=projectCard>';
           domString += `<img src="${projectCards.screenshot}"/>`;
           domString += '<header>';
           domString += `<h4>${projectCards.title}</h4>`;
           domString += '</header>';
           domString += '<div>';
+          domString += `<h6>${projectCards.description}</h6>`
           domString += `<p>${projectCards.technologiesUsed}</p>`;
           domString += `<div class="${projectCards.available}"></div>`;
-          domString += `<a href="${projectCards.url}">Link to Project</a>`;
-          domString += '<br>';
-          domString += `<a href="${projectCards.githuburl}">Link to GitHub</a>`;
+          domString += `<a class="text-dark" href="${projectCards.url}">View Project</a> || <a class="text-dark" href="${projectCards.githuburl}">View on GitHub <i class="fab fa-github"></i></a>`;
           domString += '</div>';
           domString += '</div>';
         }
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -6,7 +6,7 @@ import utils from './helpers/utils';
 const addDateToFooter = () => {
   const date = new Date().getFullYear();
   let domString = '';
-  domString += `<p class="col-xs-1 text-center text-white">Nielsen ${date}</p>`;
+  domString += `<p class="col-xs-1 m-3 text-center text-white">Nielsen ${date}</p>`;
   utils.printToDom('addMomentTimeStamp', domString);
 };
 
